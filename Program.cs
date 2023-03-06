25391d94f6acf56c9fc40e1803ea391d31efb338
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-06 09:14:25 +0200

    2 ое задание

--- Program.cs
+++ Program.cs
@@ -10,13 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            for(int i=1; i<=10;i++)
-            {
-                int x = random.Next(-40, 40);
-                Console.WriteLine(x);
-            }
-            Console.Read();
+            //Random random = new Random();
+            //for(int i=1; i<=10;i++)
+            //{
+            //    int x = random.Next(-40, 40);
+            //    Console.WriteLine(x);
+            //}
+            //Console.Read();
+
+            //int count = 0;
+            //Random random = new Random();
+            //for (int i = 1; i <= 15; i++)
+            //{
+            //    double x = -20 + 50 * random.NextDouble();
+            //    Console.WriteLine(x);
+            //    if (x > 0)
+            //    { count++; }
+            //}
+            //Console.WriteLine($"Кол-во чисел= {count}");
+            //Console.Read();
+        
+        
         }
     }
 }
