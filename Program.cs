a238c7d9864b835a1c55126e7393b43e314387fd
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-06 08:52:41 +0200

    1 задание

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-        
+            Random random = new Random();
+            for(int i=1; i<=10;i++)
+            {
+                int x = random.Next(-40, 40);
+                Console.WriteLine(x);
+            }
+            Console.Read();
         }
     }
 }
